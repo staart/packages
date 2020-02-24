a41a1543c738e2273080a80389f01c487cad90b4
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-24 20:14:45 +0100

    :sparkles: Add disposable emails working package

--- packages/disposable-email/index.ts
+++ packages/disposable-email/index.ts
@@ -1,1 +1,30 @@
-export default () => "example";
+import disposableDomains from "disposable-email-domains/index.json";
+import wildcardDomains from "disposable-email-domains/wildcard.json";
+import { joiValidate } from "@staart/validate";
+import Joi from "@hapi/joi";
+import { DISPOSABLE_EMAIL } from "@staart/errors";
+import { isMatch } from "matcher";
+
+/**
+ * Check if an email is disposable
+ * @param email - Email to check
+ */
+export const checkIfDisposableEmail = (email: string) => {
+  let isDisposable = false;
+  joiValidate(
+    {
+      email: Joi.string()
+        .email()
+        .required()
+    },
+    { email }
+  );
+  const domain = email.split("@")[1];
+  if (disposableDomains.includes(domain)) throw new Error(DISPOSABLE_EMAIL);
+  const potentialMatches = wildcardDomains.filter(w => domain.includes(w));
+  potentialMatches.forEach(
+    d => (isDisposable = isDisposable || isMatch(email, `*.${d}`))
+  );
+  if (isDisposable) throw new Error(DISPOSABLE_EMAIL);
+  return;
+};
