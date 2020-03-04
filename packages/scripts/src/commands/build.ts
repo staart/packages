5aa3b7d8349ba2c53446643dfe7e1293e3de0d4c
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-04 12:19:48 +0100

    :sparkles: Add async middleware to controllers

--- packages/scripts/src/commands/build.ts
+++ packages/scripts/src/commands/build.ts
@@ -1,5 +1,11 @@
 import { Command } from "@oclif/command";
 import { exec, touch, mkdir, cp } from "shelljs";
+import { join, resolve } from "path";
+import { readFile, writeFile } from "fs-extra";
+import recursive from "recursive-readdir";
+import { success } from "@staart/errors";
+
+const SRC = resolve(join(".staart", "src"));
 
 export default class Build extends Command {
   static description = "build your Staart API app";
@@ -11,7 +17,57 @@ export default class Build extends Command {
     cp("-r", "src", ".staart");
     cp("-r", "static", ".staart");
     cp("package.json", ".staart");
+    await updateControllerCode();
     exec("staart controllers");
     exec("tsc");
   }
 }
+
+// https://stackoverflow.com/a/38181008/1656944
+const insert = <T = string>(arr: T[], index: number, ...newItems: T[]) => [
+  ...arr.slice(0, index),
+  ...newItems,
+  ...arr.slice(index)
+];
+
+const updateControllerCode = async () => {
+  const controllers = await recursive(join(SRC, "controllers"));
+  for await (const controller of controllers) {
+    let oneController = (await readFile(controller)).toString().split("\n");
+
+    const hasjsonAsyncResponse = oneController.findIndex(line =>
+      line.includes("jsonAsyncResponse")
+    );
+    const hasClassWrapper = oneController.findIndex(line =>
+      line.includes("ClassWrapper")
+    );
+
+    if (hasjsonAsyncResponse === -1) {
+      oneController = insert(
+        oneController,
+        0,
+        `import { jsonAsyncResponse } from "@staart/server";`
+      );
+    }
+
+    if (hasClassWrapper === -1) {
+      oneController = insert(
+        oneController,
+        0,
+        `import { ClassWrapper } from "@staart/server";`
+      );
+    }
+
+    const controllerIndex = oneController.findIndex(line =>
+      line.startsWith("@Controller")
+    );
+    oneController = insert(
+      oneController,
+      controllerIndex + 1,
+      `@ClassWrapper(jsonAsyncResponse)`
+    );
+
+    await writeFile(controller, oneController.join("\n"));
+  }
+  success("Added middleware to controllers");
+};
