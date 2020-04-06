680f26cd7cad886ea3546c9343c80d60b65fdd68
Anand Chowdhary <mail@anandchowdhary.com>
2020-04-07 04:53:00 +0530

    :sparkles: Support for non-AWS ES

--- packages/elasticsearch/index.ts
+++ packages/elasticsearch/index.ts
@@ -10,20 +10,31 @@ const AWS_ELASTIC_ACCESS_KEY = process.env.AWS_ELASTIC_ACCESS_KEY || "";
 const AWS_ELASTIC_SECRET_KEY = process.env.AWS_ELASTIC_SECRET_KEY || "";
 const AWS_ELASTIC_HOST = process.env.AWS_ELASTIC_HOST || "";
 
+let elasticSearchEnabled = false;
+if (AWS_ELASTIC_HOST || ELASTIC_HOST) elasticSearchEnabled = true;
+
+export { elasticSearchEnabled };
+
+const elasticSearchConfig = AWS_ELASTIC_HOST
+  ? {
+      node: AWS_ELASTIC_HOST,
+      Connection: AmazonConnection,
+      awsConfig: {
+        credentials: {
+          accessKeyId: AWS_ELASTIC_ACCESS_KEY,
+          secretAccessKey: AWS_ELASTIC_SECRET_KEY,
+        },
+      },
+    }
+  : {
+      node: ELASTIC_HOST,
+    };
+
 /**
  * Client doesn't support the "awsConfig" property,
  * which is part of "aws-elasticsearch-connector"
  */
-export const elasticSearch = new (Client as any)({
-  node: AWS_ELASTIC_HOST || ELASTIC_HOST,
-  Connection: AmazonConnection,
-  awsConfig: {
-    credentials: {
-      accessKeyId: AWS_ELASTIC_ACCESS_KEY,
-      secretAccessKey: AWS_ELASTIC_SECRET_KEY
-    }
-  }
-}) as Client;
+export const elasticSearch = new (Client as any)(elasticSearchConfig) as Client;
 
 export const cleanElasticSearchQueryResponse = (
   response: any,
@@ -34,7 +45,7 @@ export const cleanElasticSearchQueryResponse = (
     const data = response.hits.hits;
     const newResponse: any = {
       data,
-      count
+      count,
     };
     if (count > data.length && data.length === size) {
       newResponse.hasMore = true;
