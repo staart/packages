8406f7a3cb61ede9b96e1f63a31de01506bef592
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-31 14:55:56 +0530

    :sparkles: Add update dependency script

--- scripts/update-dependencies.ts
+++ scripts/update-dependencies.ts
@@ -1,10 +1,15 @@
-import {} from "shelljs";
+import { cd, exec } from "shelljs";
 import { join } from "path";
 import { readdir } from "fs-extra";
 
 const updateDependencies = async () => {
-  const packages = await readdir(join(".", "packages"));
-  console.log(packages);
+  const packages = await readdir(join("..", "packages"));
+  for await (const dir of packages) {
+    console.log(`Updating dependencies for @staart/${dir}`);
+    cd(join("..", "packages", dir));
+    exec("pnpx updates -ui && pnpm i");
+    cd("../../scripts");
+  }
 };
 
 updateDependencies();
